2d41925cf593e7d480d982ddebc378a3ff1e48d1
edcarcruz <edcarcruz@gmail.com>
2024-03-02 16:25:32 -0500

    secret_key added to .env

--- filmfanatics2/settings.py
+++ filmfanatics2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n432jad%sd0!yl$1k*ze-jdcj37-*!axtb^1noh0#^7@(%0@x%'
+SECRET_KEY = config('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
